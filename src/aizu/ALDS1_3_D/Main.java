dd1441e50abc1a03a467c5be3fd79b1cd5450a40
yy05853 <c05853yy@gmail.com>
2021-08-19 02:00:00 +0900

    commit

--- src/aizu/ALDS1_3_D/Main.java
+++ src/aizu/ALDS1_3_D/Main.java
@@ -13,8 +13,11 @@ public class Main {
 	public static int index2;
 
 	public static void main(String[] args) {
+		//入力
 		Scanner sc = new Scanner(System.in);
 		String inStr = sc.next();
+
+		//for文でまわすために配列に変換
 		char[] inChar = inStr.toCharArray();
 
 		//初期化
@@ -26,36 +29,79 @@ public class Main {
 		for(int i = 0; i < inStr.length(); i++) {
 			switch(inChar[i]) {
 			case '\\':
+				//indexをStack1にpushして終わり
 				pushStack1(i);
 				break;
 			case '/':
+				//Stack1からindexを取り出す
 				int pop1 = popStack1();
 
 				if(pop1 != -1) {
+					//対応する'\\'があったらStack2からpopする
 					int[] pop2 = popStack2();
 					if(pop2[0] == -1) {
+						//Stack2が空だったら、
+						//今見つけた水たまりの情報をpushする
 						pushStack2(pop1, i-pop1);
 					} else if(pop1 >= pop2[0]) {
+						//Stack2が空でない、
+						//かつ過去に見つけた水たまりと繋がっていなかったら
+
+						//popした過去の水たまりの情報をそのままStack2に戻す
 						pushStack2(pop2[0], pop2[1]);
+						//今見つけた水たまりの情報をpushする
 						pushStack2(pop1, i-pop1);
 					} else {
-						while(pop1 != -1) {
-							pushStack2(pop1, i-pop1+pop2[1]);
-							pop1 = popStack1();
-							if(pop1 != -1) {
-								pop2 = popStack2();
+						//Stack2が空でない、
+						//かつ過去に見つけた水たまりと繋がっていたら
 
+						//過去の水たまりの合計用
+						int pastArea = 0;
+
+						//水たまりが繋がらないところまでさかのぼる
+						while(pop2[0] != -1) {
+							if(pop1 >= pop2[0]) {
+								//水たまりが繋がらないところまで行きついたら
+
+								//何もしない
+								break;
+							} else {
+								//繋がる水たまりを見つけたら
+
+								//過去の水たまりの面積を合計する
+								pastArea += pop2[1];
 							}
+							//さらに水たまりの情報をさかのぼる
+							pop2 = popStack2();
 						}
+						if(pop2[0] != -1) {
+							//繋がっていない水たまりの情報をそのままStack2に戻す
+							pushStack2(pop2[0], pop2[1]);
+						}
+						//過去の水たまりの面積と今の水たまりの情報をマージしてpushする
+						pushStack2(pop1, i-pop1+pastArea);
 					}
 
 				}
+				//index==-1だったら何もしない(対応する'\\'がないから)
 				break;
 			}
+			//'_'のときは何もしない
 		}
+
+		//出力
+		int sum = 0;
+		int num = 0;
 		for(int i = 0; i < index2; i++) {
-			System.out.println(stack2[i][1]);
+			sum += stack2[i][1];
+			num++;
 		}
+		System.out.println(sum);
+		System.out.print(num);
+		for(int i = 0; i < index2; i++) {
+			System.out.print(" " + stack2[i][1]);
+		}
+		System.out.println();
 	}
 
 	public static void pushStack1(int index) {
